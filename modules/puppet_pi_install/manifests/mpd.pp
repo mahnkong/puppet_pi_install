6bc961acfdee1604a41aa842e1fa3f1170d81abd
Andreas Mahnke <mahnkong@gmx.de>
2020-02-16 14:21:28 +0100

    mpd stuff

--- modules/puppet_pi_install/manifests/mpd.pp
+++ modules/puppet_pi_install/manifests/mpd.pp
@@ -2,6 +2,13 @@ class puppet_pi_install::mpd {
   package {'mpd':, ensure => "installed"}
   package {'mpc':, ensure => "installed"}
 
+  exec { 'Link /var/lib/mpd':
+    command => 'rm -rf /mnt/hdd/mpd; mv /var/lib/mpd /mnt/hdd/mpd; ln -s /mnt/hdd/mpd /var/lib/mpd',
+    path    => '/bin',
+    unless  => 'test -L /var/lib/mpd',
+    notify =>  Exec['/bin/systemctl stop mpd'],
+  }
+
   file {'/etc/mpd.conf':
     ensure =>  present,
     notify =>  Exec['/bin/systemctl stop mpd'],
